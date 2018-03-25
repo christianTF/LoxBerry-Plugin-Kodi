6cb6845407311b781e9aab4fe172f7f17cfe61a7
Christian Fenzl <fenzl@t-r-t.at>
2018-03-25 20:56:38 +0200

    Use user kodi

--- postroot.sh
+++ postroot.sh
@@ -25,23 +25,36 @@
 	# chown root:root $LBHOMEDIR/system/cron/cron.d/$2
 # fi
 
+echo "<INFO> Creating user kodi"
+# useradd kodi
+useradd -d /home/kodi kodi
+echo "<INFO> Creating group input"
+addgroup --system input
+
+
 # Add loxberry user to groups
 echo "<INFO> Adding loxberry to groups audio, video, input, dialout, plugdev, tty"
-usermod -a -G audio loxberry
-usermod -a -G video loxberry
-usermod -a -G input loxberry
-usermod -a -G dialout loxberry
-usermod -a -G plugdev loxberry
-usermod -a -G tty loxberry
+usermod -a -G audio kodi
+usermod -a -G video kodi
+usermod -a -G input kodi
+usermod -a -G dialout kodi
+usermod -a -G plugdev kodi
+usermod -a -G tty kodi
 
 # Install Kodi as a service
 echo "<INFO> Copy init.d startup script"
 cp -f data/kodi /etc/init.d/kodi
 chmod +x /etc/init.d/kodi
 echo "<INFO> Install Kodi to run automatically at startup"
+systemctl daemon-reload
 systemctl enable kodi
 
 echo "<INFO> Setting GPU memory to 192MB in config.txt"
 awk -v s="gpu_mem=192" '/^gpu_mem=/{$0=s;f=1} {a[++n]=$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' /boot/config.txt
+
+echo "<INFO> Creating Kodi settings directory"
+mkdir -p /home/kodi/.kodi/userdata
+cp data/advancedsettings.xml /home/kodi/.kodi/userdata
+chown -R kodi /home/kodi
 
 exit 0
